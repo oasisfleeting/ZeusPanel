927704e4aeeae7bb651ab11212fc4e3c1650cd2f
Zeusking19 <funnyparrotfish@virginmedia.com>
2013-03-31 19:14:10 +0100

    Started folder structure, main landing page done

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>ZeusPanel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="global/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 20px;
+        padding-bottom: 40px;
+      }
+
+      /* Custom container */
+      .container-narrow {
+        margin: 0 auto;
+        max-width: 700px;
+      }
+      .container-narrow > hr {
+        margin: 30px 0;
+      }
+
+      /* Main marketing message and sign up button */
+      .jumbotron {
+        margin: 60px 0;
+        text-align: center;
+      }
+      .jumbotron h1 {
+        font-size: 72px;
+        line-height: 1;
+      }
+      .jumbotron .btn {
+        font-size: 21px;
+        padding: 14px 24px;
+      }
+
+      /* Supporting marketing content */
+      .marketing {
+        margin: 60px 0;
+      }
+      .marketing p + h4 {
+        margin-top: 28px;
+      }
+    </style>
+    <link href="global/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="container-narrow">
+
+      <div class="masthead">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="about.php">About Us</a></li>
+          <li><a href="billing/register.php">Register</a></li>
+		  <li><a href="panel/login.php">Login</a></li>
+        </ul>
+        <h3 class="muted">ZeusPanel</h3>
+      </div>
+
+      <hr>
+
+      <div class="jumbotron">
+        <h1>First class web hosting!</h1>
+        <p class="lead">Put some info about your services here!</p>
+        <a class="btn btn-large btn-success" href="billing/register.php">Sign up today</a>
+      </div>
+
+      <hr>
+
+      <div class="row-fluid marketing">
+        <div class="span6">
+          <h4>99.9% Uptime</h4>
+          <p>Our system is reliable and easy to use, causing no downtime during installation and day to day running.</p>
+
+          <h4>24/7 Support</h4>
+          <p>With ZeusPanel's great support technologies, your customers are guarunteed the best service possible.</p>
+
+          <h4>Flexible Plans</h4>
+          <p>Have full control over your price plans. ZeusPanel's WHM (Web Host Manager) interface helps administrators get the job done.</p>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="footer">
+        <p>Company Name - Powered by ZeusPanel</p>
+      </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="global/js/jquery.js"></script>
+    <script src="global/js/bootstrap-transition.js"></script>
+    <script src="global/js/bootstrap-alert.js"></script>
+    <script src="global/js/bootstrap-modal.js"></script>
+    <script src="global/js/bootstrap-dropdown.js"></script>
+    <script src="global/js/bootstrap-scrollspy.js"></script>
+    <script src="global/js/bootstrap-tab.js"></script>
+    <script src="global/js/bootstrap-tooltip.js"></script>
+    <script src="global/js/bootstrap-popover.js"></script>
+    <script src="global/js/bootstrap-button.js"></script>
+    <script src="global/js/bootstrap-collapse.js"></script>
+    <script src="global/js/bootstrap-carousel.js"></script>
+    <script src="global/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
---
+++ panel/login.php
@@ -0,0 +1,12 @@
+<?
+//ZeusPanel - Client login
+//This code tells the difference between user and reseller.
+//Note that administrators cannot login here.
+?>
+<html>
+<head>
+<title>ZeusPanel - Client Login</title>
+</head>
+<body>
+</body>
+</html>
