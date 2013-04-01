c0a284e9a3c92219f4b93b0a0b4b045f86b8eddf
Zeusking19 <funnyparrotfish@virginmedia.com>
2013-04-01 12:06:04 +0100

    Added login code

--- admin/index.php
+++ admin/index.php
@@ -25,6 +25,49 @@ body {
 
 <div id="login">
 <center>
+<?php
+session_start();
+
+// Check if he wants to login:
+if (!empty($_POST[username]))
+{
+	require_once("../global/includes/db_config.php");
+
+    $username = mysql_escape_string($_POST[username]);
+    $password = substr(sha1(mysql_escape_string($_POST[password])),0,33);
+	// Check if he has the right info.
+	$query = mysql_query("SELECT username, usertype FROM users
+							WHERE username = '$username'
+							AND password = '$password'")
+	or die ("Error - Couldn't login user.");
+	
+	$row = mysql_fetch_assoc($query);
+	$username = $row["username"];
+	$usertype = $row["rank"];
+	
+	if (!empty($username)) // he got it.
+	{
+		//Now we have to check what rank the person is.
+		// 1 = User
+		// 2 = Reseller
+		// 3 = Administrator
+		if(intval($rank) > 2){
+		$_SESSION['adminusername'] = $username;
+		$_SESSION[rank] = $rank;
+		header('Location: main.php');
+		exit();
+		} else {
+			echo "Your account is not authorized to access WHM.";	
+		}
+	}
+	else // bad info.
+	{
+		echo "Error - Couldn't login user.<br /><br />
+			Please try again.";
+		exit();
+	}
+}
+?>
   <form id="form1" name="form1" method="post" action="">
     <p>
       <label for="username">Username:</label>
