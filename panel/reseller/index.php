89c6b3d0927fc978e21077f7fb632f94540ed43d
Zeusking19 <funnyparrotfish@virginmedia.com>
2013-04-02 09:18:37 +0100

    Started Reseller Panel

---
+++ panel/reseller/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ZeusPanel</title>
+<style type="text/css">
+#container {
+	width: 745px;
+	margin-right: auto;
+	margin-left: auto;
+}
+#container #header {
+	background-image: url(img/header.png);
+	height: 195px;
+	background-repeat: no-repeat;
+}
+#home {
+	position: absolute;
+	left: 239px;
+	top: 151px;
+}
+</style></head>
+
+<body>
+<div id="container">
+  <div id="header"><a href="index.php"><img src="img/home.png" width="51" height="52" alt="home" id="home" style="border-style: none"/></a></div>
+  <p>Container content</p>
+</div>
+</body>
+</html>
