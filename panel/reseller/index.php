e7c350b82d530d7448d2b5e6e3d0729871ddd502
Zeusking19 <funnyparrotfish@virginmedia.com>
2013-04-02 09:29:04 +0100

    Finished the base style of the control panel

--- panel/reseller/index.php
+++ panel/reseller/index.php
@@ -8,6 +8,7 @@
 	width: 745px;
 	margin-right: auto;
 	margin-left: auto;
+	background-color: #E2E2E2;
 }
 #container #header {
 	background-image: url(img/header.png);
@@ -21,12 +22,22 @@
 #home {
 	margin-left: auto;
 }
+#container #footer {
+	background-image: url(img/footer.png);
+	height: 52px;
+	color: #FFF;
+	font-family: Arial;
+	padding-top: 15px;
+	background-repeat: no-repeat;
+	background-color: #FFF;
+}
 </style></head>
 
 <body>
 <div id="container">
   <div id="header"><a href="index.php"><img src="img/home.png" alt="home" name="home" width="51" height="52" id="home" style="border-style: none"/></a></div>
   <p>Container content</p>
+  <div id="footer"><center>Powered by ZeusPanel</center></div>
 </div>
 </body>
 </html>
